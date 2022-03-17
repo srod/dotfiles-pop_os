43af468b364cc810f1bff89c49c0a6d3d316725d
Rodolphe Stoclin <srodolphe@gmail.com>
2022-03-17 10:58:34 +0100

    Update

--- src/shell/zsh/aliases/aliases.zsh
+++ src/shell/zsh/aliases/aliases.zsh
@@ -15,10 +15,6 @@ alias npmup="npm -g update; npm install -g npm"
 alias update="aptup; npmup"
 
 # Utils
-alias clean.ds_store="find . -type f -name '*.DS_Store' -ls -delete"
 alias clean.node="rm -rf node_modules"
 alias clean.npm="clean.node && npm cache verify && npm install"
 alias clean.yarn="clean.node && yarn"
-alias dropbox.conflicted="find ~/Dropbox/ -name '*Copie en conflit*' && find ~/Dropbox/ -name '*Conflict*' && find ~/Dropbox/ -name '*conflict*'"
-alias json="python -mjson.tool"
-weather() { curl -4 wttr.in/${1:-bussy-saint-georges} }
